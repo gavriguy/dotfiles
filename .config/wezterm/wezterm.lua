9f3f0adfd086c0228b627bd7ca92b514e3faed82
gavriguy <gavri.guy@gmail.com>
2024-02-19 07:47:16 +0200

    first commit

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local act = wezterm.action
+
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.font_size = 16
+
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'k',
+    mods = 'SUPER',
+    action = act.ClearScrollback 'ScrollbackAndViewport',
+  }
+}
+
+-- and finally, return the configuration to wezterm
+return config
